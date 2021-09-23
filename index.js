7b1290c3b139b80eedc384dcb71717b68fe6275e
Dimitri Chenet <dchenet1017@gmail.com>
2021-09-23 07:52:35 -0500

    commit tree 961c971daf33c30d2f6df9861751c60ff666a5ec

--- index.js
+++ index.js
@@ -3,30 +3,22 @@
 const employee = {
     name: `Sam`,
     streetAddress: `11 broadway`
+    
 };
 
 function updateEmployeeWithKeyAndValue (employee,key, value) {
-    return Object.assign ({}, employee, { [key]: value });
+    console.log (employee)
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value) {
-    employee[key] = value;
-
-    return employee;
+   
 }
 
 function deleteFromEmployeeByKey(employee, key) {
 
-    const newobj = Object.assign({}, employee);
-    
-    delete newobj[key];
-
-    return newobj;
 
 }
 
 function destructivelyDeleteFromEmployeeByKey (employee, key) {
-    delete employee [key];
-
-    return employee;
+    
 }
